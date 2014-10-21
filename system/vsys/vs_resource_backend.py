01984e7c3553537c3854ed3bde02f778bde5b746
Stephen Soltesz <stephen.soltesz@gmail.com>
2014-10-21 02:46:09 +0000

    Add vs_resource_backend and unit tests.
    This patch includes vs_resource_backend and unit tests.
    The vs_resource_backend is the vsys backend for the collectd-mlab
    plugin.

---
+++ system/vsys/vs_resource_backend.py
@@ -0,0 +1,359 @@
+#!/usr/bin/python
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+The vs_resource_backend is the vsys backend for collectd-mlab plugin.
+
+The collectd-mlab plugin runs in a vserver context, and the vs_resource_backend
+runs in the host context of an M-Lab server. This script is needed to provide
+access to system information not otherwise accessible from inside a vserver
+context.
+
+When vsys detects an open on the vsys frontend FIFOs, this script is executed.
+Stdin and stdout are connected to the FIFOs used by the frontend (in /vsys/*).
+The vs_resource_backend does not exit as long as the frontend keeps the FIFOs
+open (e.g. stdin / stdout are open). The vs_resource_backend exits when
+communication with the frontend is no longer possible (e.g. due to EOF, or
+EPIPE).
+
+The vs_resource_backend reads commands from stdin separated by a newline. The
+vs_resource_backend writes replies to stdout separated by a newline.
+
+Command format:
+  <command><NL>
+
+Reply format:
+  <json reply><NL>
+
+The vs_resource_backend supports three commands:
+  vs_xid_names: returns a mapping from xid to slice names.
+  vs_xid_dlimits: returns a mapping from xid to storage quota usage.
+  backend_stats: returns memory and cpu usage statistics for this process.
+
+The JSON reply has some standard attributes:
+  version: int, the version of this backend script.
+  ts: float, timestamp of request.
+  message_type: str, the name of the command in request.
+  data: obj, the result of the command.
+
+== Installation ==
+
+In the host context, install vs_resource_backend.py (without .py extension) to:
+
+    /vsys/vs_resource_backend
+
+The mlab_utility slice (or another slice that should access this backend)
+should have a PlanetLab slice attribute for this script:
+
+    vsys=vs_resource_backend
+"""
+
+import ctypes
+import ctypes.util
+import errno
+import json
+import os
+import pwd
+import sys
+import syslog
+import time
+import traceback
+
+
+VS_BACKEND_VERSION = 1
+
+_LIBVSERVER = None
+_MAX_MESSAGE_LENGTH = 128
+_PAGESIZE = os.sysconf(os.sysconf_names['SC_PAGESIZE'])
+_PROC_PID_STAT = None
+_VS_PREFIX_DIR = "/proc/virtual"
+
+
+class Error(Exception):
+  """Base for errors in this module."""
+
+
+class EndOfFileError(Error):
+  """Raised when receiving an EOF from stdin."""
+
+
+class LibVserverError(Error):
+  """Raised when an error occurs with libvserver."""
+
+
+# Invalid class name. This is a special case needed for a C-data type.
+# pylint: disable=C0103
+class vc_ctx_dlimit(ctypes.Structure):
+  """A ctypes representation of vserver, struct vc_ctx_dlimit."""
+  _fields_ = [('space_used', ctypes.c_uint32),
+              ("space_total", ctypes.c_uint32),
+              ("inodes_used", ctypes.c_uint32),
+              ("inodes_total", ctypes.c_uint32),
+              ("reserved", ctypes.c_uint32),]
+# pylint: enable=C0103
+
+
+def init_libvserver():
+  """Loads the libvserver library into the global _LIBVSERVER.
+
+  Returns:
+     True on success, False on failure.
+  """
+  global _LIBVSERVER
+  lib = ctypes.util.find_library("vserver")
+  if lib is None:
+    syslog_err('failed to find vserver library')
+    return False
+
+  try:
+    _LIBVSERVER = ctypes.cdll.LoadLibrary(lib)
+  except OSError as err:
+    syslog_err('failed to load vserver library: %s' % err)
+    return False
+
+  return True
+
+
+def vc_get_dlimit(filename, xid):
+  """Calls libvserver.vc_get_dlimit to get dlimit values for xid.
+
+  Dlimits are applied to a specific directory path.
+  
+  Args:
+    filename: str, path to slice root dir, where quota limits are applied.
+    xid: int, xid of vserver.
+  Returns:
+    list of int, [space_used, space_total, inodes_used, inodes_total, reserved]
+  """
+  if _LIBVSERVER is None:
+    if not init_libvserver():
+      return [0, 0, 0, 0, 0]
+
+  c_limit = vc_ctx_dlimit()
+  c_filename = ctypes.c_char_p(filename)
+  c_xid = ctypes.c_uint32(xid)
+
+  err = _LIBVSERVER.vc_get_dlimit(
+      c_filename, c_xid, ctypes.c_uint32(0), ctypes.byref(c_limit))
+
+  if err == -1:
+    raise LibVserverError('Failed to get dlimits for %s' % xid)
+
+  # '-1' means no quota.
+  ret = [c_limit.space_used,
+         c_limit.space_total,
+         c_limit.inodes_used,
+         -1 if 2**32-1 == c_limit.inodes_total else c_limit.inodes_total,
+         c_limit.reserved]
+  return ret
+
+
+def syslog_err(msg):
+  """Logs msg to syslog."""
+  for message in msg.split('\n'):
+    if message:
+      syslog.syslog(syslog.LOG_ERR, 'vs_resource_backend: %s' % message)
+
+
+def get_xid_names():
+  """Fulfills the vs_xid_names request.
+
+  Returns:
+    dict, keys are vserver xids as string, values are vserver names as string.
+  """
+  vs_prefix = _VS_PREFIX_DIR
+  xid_names = {}
+
+  for entry in os.listdir(vs_prefix):
+    entry_path = os.path.join(vs_prefix, entry)
+    if not os.path.isdir(entry_path):
+      continue
+
+    try:
+      # On PlanetLab xid is set to uid.
+      xid = int(entry)
+    except ValueError:
+      syslog_err("Failed to convert expected xid: %s" % entry)
+      continue
+
+    try:
+      pw_entry = pwd.getpwuid(xid)
+    except KeyError:
+      # This is serious. A vserver is running without passwd entry.
+      syslog_err("Failed to find /etc/passwd entry for xid: %s" % entry)
+      continue
+
+    vs_name = pw_entry.pw_name
+    if not vs_name:
+      syslog_err("pw_entry.pw_name is zero length for xid: %s" % entry)
+      continue
+
+    xid_names[entry] = vs_name
+
+  return xid_names
+
+
+def get_xid_dlimits():
+  """Fulfills the vs_xid_dlimits request.
+
+  Returns:
+    dict, keys are xids as string, values are 5-element lists with dlimits.
+  """
+  vs_prefix = _VS_PREFIX_DIR
+  xid_dlimits = {}
+
+  for entry in os.listdir(vs_prefix):
+    entry_path = os.path.join(vs_prefix, entry)
+    if not os.path.isdir(entry_path):
+      continue
+
+    try:
+      xid = int(entry)
+    except ValueError:
+      continue
+    
+    try:
+      dlim = vc_get_dlimit("/vservers", xid)
+    except LibVserverError as err:
+      # Errors calling vc_get_dlimit are likely to end fatally.
+      syslog_err('vc_get_dlimit failed: %s' % err)
+      continue
+
+    xid_dlimits[entry] = dlim
+
+  return xid_dlimits
+
+
+def get_backend_stats(stat_path):
+  """Fulfills the backend_stats request.
+
+  Returns:
+    dict, with process time and memory usage stats. Includes:
+        utime: user time, cumulative USER_HZ.
+        stime: system time, cumulative USER_HZ.
+        vsize: virtual memory size, bytes,
+        rss: resident set size, bytes.
+  """
+  index_utime = 13
+  index_stime = 14
+  index_cutime = 15
+  index_cstime = 16
+  index_vsize = 22
+  index_rss = 23
+  index_max = 24
+
+  backend_stats = {'utime': 0, 'stime': 0, 'vsize': 0, 'rss': 0}
+
+  if not os.path.exists(stat_path):
+    return backend_stats
+
+  try:
+    stat_fields = open(stat_path, 'r').read().strip().split()
+  except IOError as err:
+    syslog_err('get_backend_stats: failed to read stats: %s' % str(err))
+    return backend_stats
+
+  if len(stat_fields) < index_max:
+    syslog_err('get_backend_stats found only %s fields.' % len(stat_fields))
+    return backend_stats
+
+  try:
+    backend_stats['utime'] = (
+      float(stat_fields[index_utime]) + float(stat_fields[index_cutime]))
+    backend_stats['stime'] = (
+        float(stat_fields[index_stime]) + float(stat_fields[index_cstime]))
+    backend_stats['vsize'] = int(stat_fields[index_vsize])
+    backend_stats['rss'] = int(stat_fields[index_rss]) * _PAGESIZE
+  except ValueError as err:
+    syslog_err('get_backend_stats failed to convert stats: %s' % str(err))
+    return backend_stats
+
+  return backend_stats
+
+
+def report(obj):
+  """Returns a dict that wraps the given object with additional metadata."""
+  data = {
+    'version': VS_BACKEND_VERSION,
+    'ts': time.time(),
+    'data': obj,
+    'message_type': '',
+  }
+  return data
+
+
+def handle_message(message_type):
+  """Dispatches message type to right handler and returns json result."""
+  if message_type == 'vs_xid_names':
+    raw_data = get_xid_names()
+  elif message_type == 'vs_xid_dlimits':
+    raw_data = get_xid_dlimits()
+  elif message_type == 'backend_stats':
+    raw_data = get_backend_stats(_PROC_PID_STAT)
+  else:
+    syslog_err('Unknown message type: %s' % message_type)
+    return None
+
+  data = report(raw_data)
+  data['message_type'] = message_type
+  return json.dumps(data)
+
+
+def handle_request():
+  """Handles request by reading from stdin, and dispatching request."""
+
+  line = sys.stdin.readline(_MAX_MESSAGE_LENGTH)
+  if not line:
+    raise EndOfFileError('Received EOF. Exiting.')
+
+  if line[-1] != '\n':
+    syslog_err('Ignoring incomplete message: %s' % line)
+    return 
+
+  json_data = handle_message(line.strip())
+  if not json_data:
+    return 
+
+  sys.stdout.write('%s\n' % json_data)
+  sys.stdout.flush()
+
+
+def main():
+  global _PROC_PID_STAT
+  _PROC_PID_STAT = '/proc/%s/stat' % os.getpid()
+
+  try:
+    while True:
+      # Loop forever, until handle_request raises an exception.
+      handle_request()
+  except EndOfFileError as err:
+    # EOF on stdin is a clean exit, likely due to a frontend close.
+    exit_code = 0
+    exit_message = ''
+  except IOError as err:
+    # EPIPE is the only ok reason to exit. The rest are unexpected.
+    exit_code = 0 if err.errno == errno.EPIPE else 1
+    exit_message = '' if err.errno == errno.EPIPE else str(err)
+  except Exception as err:
+    # And any other exception type, log all the details.
+    exit_code = 1
+    exit_message = traceback.format_exc()
+  finally:
+    if exit_message:
+      syslog_err(exit_message)
+    sys.exit(exit_code)
+
+
+if __name__ == "__main__":  # pragma: no cover
+  main()
---
+++ system/vsys/vs_resource_backend_test.py
@@ -0,0 +1,342 @@
+"""Unit tests for vsys vs_resource_backend script."""
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import errno
+import pwd
+import unittest
+
+# third-party
+import mock
+
+# module under test.
+import vs_resource_backend
+
+# R0904: too many public methods.
+# W0212: access to a protected member, required to access global values in
+#        module under test.
+# pylint: disable=R0904
+# pylint: disable=W0212
+
+
+def fake_vc_get_dlimit(
+    unused_c_filename, unused_c_xid, unused_c_flags, c_limit):
+  """Fake version of vserver library method vc_get_dlimit."""
+  limit = c_limit._obj
+  limit.space_used = 10
+  limit.space_total = 100
+  limit.inodes_used = 20
+  limit.inodes_total = 2**32-1
+  limit.reserved = 2
+
+
+class MlabVsResourceBackendTests(unittest.TestCase):
+
+  def setUp(self):
+    global vs_resource_backend
+    vs_resource_backend = reload(vs_resource_backend)
+    self._testdata_dir = os.path.join(
+        os.path.dirname(vs_resource_backend.__file__), 'testdata')
+
+  def testunit_vc_get_dlimit(self):
+    mock_vslib = mock.Mock()
+    mock_vslib.vc_get_dlimit = fake_vc_get_dlimit
+    vs_resource_backend._LIBVSERVER = mock_vslib
+    expected_result = [10, 100, 20, -1, 2]
+
+    returned_result = vs_resource_backend.vc_get_dlimit('/vservers', 515)
+
+    self.assertEqual(returned_result, expected_result)
+
+  @mock.patch('vs_resource_backend.ctypes.util')
+  def testunit_vc_get_dlimit_WHEN_vserver_lib_not_found(self, mock_ctypes_util):
+    mock_ctypes_util.find_library.return_value = None
+    expected_result = [0, 0, 0, 0, 0]
+
+    returned_result = vs_resource_backend.vc_get_dlimit('/vservers', 515)
+
+    self.assertEqual(returned_result, expected_result)
+
+  @mock.patch('vs_resource_backend.ctypes.util')
+  def testunit_vc_get_dlimit_WHEN_vserver_load_fails(self, mock_ctypes_util):
+    mock_ctypes_util.find_library.return_value = 'not-a-real-library'
+    expected_result = [0, 0, 0, 0, 0]
+
+    returned_result = vs_resource_backend.vc_get_dlimit('/vservers', 515)
+
+    self.assertEqual(returned_result, expected_result)
+
+  @mock.patch('vs_resource_backend.vc_get_dlimit')
+  def testunit_get_xid_dlimits(self, mock_vc_get_dlimit):
+    vs_resource_backend._VS_PREFIX_DIR = self._testdata_dir
+    dlim = [0, 1, 2, 3, 4]
+    mock_vc_get_dlimit.return_value = dlim
+    expected_value = {'515': dlim[:]}
+
+    returned_value = vs_resource_backend.get_xid_dlimits()
+
+    self.assertEqual(expected_value, returned_value)
+
+  @mock.patch('vs_resource_backend.vc_get_dlimit')
+  def testunit_get_xid_dlimits_WHEN_vc_get_dlimit_RAISES_Exception(self,
+      mock_vc_get_dlimit):
+    mock_vc_get_dlimit.side_effect = (
+        vs_resource_backend.LibVserverError('broken system call'))
+    expected_value = {}
+
+    returned_value = vs_resource_backend.get_xid_dlimits()
+
+    self.assertEqual(expected_value, returned_value)
+
+  def testunit_get_backend_stats(self):
+    test_stat_path = os.path.join(self._testdata_dir, 'proc_pid_stat')
+    expected_value = {'rss': 1740800, 'vsize': 1234321, 'stime': 1503.0,
+                      'utime': 255.0}
+
+    returned_value = vs_resource_backend.get_backend_stats(test_stat_path)
+
+    self.assertEqual(expected_value, returned_value)
+
+  def testunit_get_backend_stats_WHEN_path_does_not_exist(self):
+    bad_stat_path = os.path.join(self._testdata_dir, 'no_proc_pid_stat')
+    expected_value = {'rss': 0, 'vsize': 0, 'stime': 0, 'utime': 0}
+
+    returned_value = vs_resource_backend.get_backend_stats(bad_stat_path)
+
+    self.assertEqual(expected_value, returned_value)
+
+  @mock.patch('__builtin__.open')
+  def testunit_get_backend_stats_WHEN_path_RAISES_IOError(self, mock_open):
+    test_stat_path = os.path.join(self._testdata_dir, 'proc_pid_stat')
+    mock_open.side_effect = IOError(-1, 'Forced IO error')
+    expected_value = {'rss': 0, 'vsize': 0, 'stime': 0, 'utime': 0}
+
+    returned_value = vs_resource_backend.get_backend_stats(test_stat_path)
+
+    self.assertEqual(expected_value, returned_value)
+
+  def testunit_get_backend_stats_WHEN_bad_stat_data(self):
+    bad_stat_path = os.path.join(self._testdata_dir, 'bad_proc_pid_stat')
+    expected_value = {'rss': 0, 'vsize': 0, 'stime': 0, 'utime': 0}
+
+    returned_value = vs_resource_backend.get_backend_stats(bad_stat_path)
+
+    self.assertEqual(expected_value, returned_value)
+
+  def testunit_get_backend_stats_WHEN_data_RAISES_ValueError(self):
+    bad_stat_path = os.path.join(self._testdata_dir, 'bad_proc_pid_stat2')
+    expected_value = {'rss': 0, 'vsize': 0, 'stime': 0, 'utime': 0}
+
+    returned_value = vs_resource_backend.get_backend_stats(bad_stat_path)
+
+    self.assertEqual(expected_value, returned_value)
+
+  @mock.patch('vs_resource_backend.pwd')
+  def testunit_get_xid_names(self, mock_pwd):
+    vs_resource_backend._VS_PREFIX_DIR = self._testdata_dir
+    mock_pwd.getpwuid.return_value = pwd.struct_pwent(
+        ('mlab_utility', '*', 515, 505, 1, '/home/mlab_utility', '/bin/bash'))
+    expected_value = {'515': 'mlab_utility'}
+
+    returned_value = vs_resource_backend.get_xid_names()
+
+    self.assertEqual(returned_value, expected_value)
+ 
+  @mock.patch('vs_resource_backend.pwd')
+  def testunit_get_xid_names_WHEN_getpwuid_RAISES_KeyError(self, mock_pwd):
+    vs_resource_backend._VS_PREFIX_DIR = self._testdata_dir
+    mock_pwd.getpwuid.side_effect = KeyError('fake key error')
+    expected_value = {}
+
+    returned_value = vs_resource_backend.get_xid_names()
+
+    self.assertEqual(returned_value, expected_value)
+
+  @mock.patch('vs_resource_backend.pwd')
+  def testunit_get_xid_names_WHEN_pw_name_IS_invalid(self, mock_pwd):
+    vs_resource_backend._VS_PREFIX_DIR = self._testdata_dir
+    mock_pwd.getpwuid.return_value = pwd.struct_pwent(
+        ('', '*', 515, 505, 1, '/home/mlab_utility', '/bin/bash'))
+    expected_value = {}
+
+    returned_value = vs_resource_backend.get_xid_names()
+
+    self.assertEqual(returned_value, expected_value)
+
+  @mock.patch('vs_resource_backend.time')
+  def testunit_report(self, mock_time):
+    mock_time.time.return_value = 10
+    mock_obj = mock.Mock()
+    expected_value = {'version': vs_resource_backend.VS_BACKEND_VERSION,
+                      'ts': 10,
+                      'data': mock_obj,
+                      'message_type': ''}
+
+    returned_value = vs_resource_backend.report(mock_obj)
+
+    self.assertEqual(returned_value, expected_value)
+
+  @mock.patch('vs_resource_backend.syslog_err')
+  def testunit_handle_message_WHEN_type_IS_unknown(
+      self, mock_syslog_err):
+
+    returned_value = vs_resource_backend.handle_message('unknown')
+
+    mock_syslog_err.assert_called_once()
+    self.assertEqual(returned_value, None)
+
+  @mock.patch('vs_resource_backend.get_backend_stats')
+  @mock.patch('vs_resource_backend.time')
+  def testunit_handle_message_WHEN_type_IS_backend_stats(
+      self, mock_time, mock_get_backend_stats):
+    mock_time.time.return_value = 10
+    mock_get_backend_stats.return_value = {'rss': 2961408, 'vsize': 4919296,
+                                        'stime': 13.0, 'utime': 2.0}
+    expected_value = ('{"message_type": "backend_stats", "version": 1, "data":'
+                      ' {"utime": 2.0, "vsize": 4919296, "stime": 13.0, '
+                      '"rss": 2961408}, "ts": 10}')
+  
+    returned_value = vs_resource_backend.handle_message('backend_stats')
+
+    mock_get_backend_stats.assert_called_once()
+    self.assertEqual(returned_value, expected_value)
+
+  @mock.patch('vs_resource_backend.get_xid_dlimits')
+  @mock.patch('vs_resource_backend.time')
+  def testunit_handle_message_WHEN_type_IS_vs_xid_dlimits(
+      self, mock_time, mock_get_xid_dlimits):
+    mock_time.time.return_value = 10
+    mock_get_xid_dlimits.return_value = {'515': [386460, 10000000, 9533, -1, 2]}
+    expected_value = ('{"message_type": "vs_xid_dlimits", "version": 1, "data":'
+                      ' {"515": [386460, 10000000, 9533, -1, 2]}, "ts": 10}')
+  
+    returned_value = vs_resource_backend.handle_message('vs_xid_dlimits')
+
+    mock_get_xid_dlimits.assert_called_once()
+    self.assertEqual(returned_value, expected_value)
+
+  @mock.patch('vs_resource_backend.get_xid_names')
+  @mock.patch('vs_resource_backend.time')
+  def testunit_handle_message_WHEN_type_IS_vs_xid_names(
+      self, mock_time, mock_get_xid_names):
+    mock_time.time.return_value = 10
+    mock_get_xid_names.return_value = {'515': 'mlab_utility'}
+    expected_value = ('{"message_type": "vs_xid_names", "version": 1, "data":'
+                      ' {"515": "mlab_utility"}, "ts": 10}')
+  
+    returned_value = vs_resource_backend.handle_message('vs_xid_names')
+
+    mock_get_xid_names.assert_called_once()
+    self.assertEqual(returned_value, expected_value)
+
+  @mock.patch('vs_resource_backend.syslog')
+  def testunit_syslog_err(self, mock_syslog):
+    mock_syslog.LOG_ERR = 1
+    mock_syslog.syslog.side_effect = [None, None]
+    fake_message = 'one\ntwo\n'
+    expected_calls = [mock.call(1, 'vs_resource_backend: one'),
+                      mock.call(1, 'vs_resource_backend: two')]
+
+    vs_resource_backend.syslog_err(fake_message)
+
+    mock_syslog.syslog.assert_has_calls(expected_calls)
+
+  @mock.patch('vs_resource_backend.handle_message')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_handle_request(self, mock_sys, mock_handle_message):
+    mock_sys.stdin.readline.return_value = 'backend_stats\n'
+    mock_handle_message.return_value = '{"version": 1, "data": "fake reply"}'
+    expected_value = '{"version": 1, "data": "fake reply"}\n'
+
+    vs_resource_backend.handle_request()
+    
+    mock_sys.stdout.write.assert_called_with(expected_value)
+
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_handle_request_WHEN_readline_RAISES_eof(self, mock_sys):
+    mock_sys.stdin.readline.return_value = ''
+
+    self.assertRaises(vs_resource_backend.EndOfFileError,
+                      vs_resource_backend.handle_request)
+
+  @mock.patch('vs_resource_backend.syslog_err')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_handle_request_WHEN_message_IS_incomplete(
+      self, mock_sys, mock_syslog_err):
+    mock_sys.stdin.readline.return_value = 'incomplete_message_without_newline'
+
+    vs_resource_backend.handle_request()
+
+    mock_syslog_err.assert_called_once()
+
+  @mock.patch('vs_resource_backend.handle_message')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_handle_request_WHEN_handle_message_RETURNS_nothing(
+      self, mock_sys, mock_handle_message):
+    mock_sys.stdin.readline.return_value = 'token backend_stats\n'
+    mock_handle_message.return_value = None
+
+    vs_resource_backend.handle_request()
+    
+    mock_handle_message.assert_called_once()
+
+  @mock.patch('vs_resource_backend.handle_message')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_handle_request_WHEN_write_RAISES_ioerror(
+      self, mock_sys, mock_handle_message):
+    mock_sys.stdin.readline.return_value = 'token backend_stats\n'
+    expected_value = 'token {"version": 1, "data": "fake reply"}\n'
+    mock_handle_message.return_value = expected_value
+    mock_sys.stdout.write.side_effect = IOError(errno.EPIPE, 'fake ioerror')
+
+    self.assertRaises(IOError, vs_resource_backend.handle_request)
+
+  @mock.patch('vs_resource_backend.handle_request')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_main_WHEN_request_RAISES_eof(
+      self, mock_sys, mock_handle_request):
+    mock_handle_request.side_effect = (
+        vs_resource_backend.EndOfFileError('fake eof'))
+    
+    vs_resource_backend.main()
+
+    mock_handle_request.assert_called_once()
+    mock_sys.exit.assert_called_with(0)
+    
+  @mock.patch('vs_resource_backend.handle_request')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_main_WHEN_request_RAISES_ioerror(
+      self, mock_sys, mock_handle_request):
+    mock_handle_request.side_effect = IOError(-1, 'fake ioerror')
+    
+    vs_resource_backend.main()
+
+    mock_handle_request.assert_called_once()
+    mock_sys.exit.assert_called_with(1)
+
+  @mock.patch('vs_resource_backend.handle_request')
+  @mock.patch('vs_resource_backend.sys')
+  def testunit_main_WHEN_request_RAISES_other_exception(
+      self, mock_sys, mock_handle_request):
+    mock_handle_request.side_effect = Exception('fake eof')
+    
+    vs_resource_backend.main()
+
+    mock_handle_request.assert_called_once()
+    mock_sys.exit.assert_called_with(1)
+
+
+if __name__ == "__main__":
+  unittest.main()
