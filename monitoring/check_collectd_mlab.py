007583c5ae35d6d32c2ee9e94bf2741de0d56aac
Stephen Soltesz <stephen.soltesz@gmail.com>
2015-03-02 01:53:18 +0000

    Add a nagios plugin to check_collectd_mlab.
    This change includes check_collectd_mlab.py and associated tests.
    check_collectd_mlab.py is a nagios plugin that checks the installation
    and runtime health of collectd-mlab.

---
+++ monitoring/check_collectd_mlab.py
@@ -0,0 +1,369 @@
+#!/usr/bin/python
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Summary:
+  check_collectd_mlab.py is a nagios plugin that checks the health of
+  collectd-mlab.
+
+  check_collectd_mlab.py should be installed and run form the host context.
+
+When check_collectd_mlab.py returns an OKAY status, the following are true:
+ * collectd is installed in the utility slice.
+ * collectd responds to commands sent over the collectd unix socket.
+ * collectd is running on a writable filesystem.
+ * vsys configuration is correct for the backend, frontend, and slice acl.
+ * resource usage by the collectd process and slice are normal.
+ * TODO: make resource usage checks configurable.
+ * TODO: check the last disk sync time recently and without too much error.
+
+If any of the above checks fail, check_collectd_mlab.py exits with a nagios
+error code and descriptive message.
+
+Installation:
+  /usr/lib/nagios/plugins/
+
+Example usage:
+  $ ./check_collectd_mlab.py
+  OKAY: Blue skies smiling at me: 1.27 sec
+
+If collectd is not running:
+  $ ./check_collectd_mlab.py
+  CRITICAL: collectd unixsock not present:
+       /vservers/mlab_utility/var/run/collectd-unixsock.
+"""
+import os
+import signal
+import socket
+import subprocess
+import sys
+import time
+
+
+# These values expect that:
+# * This script runs in host context.
+# * collectd runs in mlab_utility slice.
+# * collectd uses default pidfile name.
+# * collectd uses default unixsock name.
+SLICENAME = 'mlab_utility'
+COLLECTD_BIN = '/vservers/%s/usr/sbin/collectd' % SLICENAME
+COLLECTD_PID = '/vservers/%s/var/run/collectd.pid' % SLICENAME
+COLLECTD_UNIXSOCK = '/vservers/%s/var/run/collectd-unixsock' % SLICENAME
+
+HOSTNAME = socket.gethostname()
+VSYSPATH_ACL = '/vsys/vs_resource_backend.acl'
+VSYSPATH_BACKEND = '/vsys/vs_resource_backend'
+VSYSPATH_SLICE = '/vservers/%s/vsys/vs_resource_backend.in' % SLICENAME
+DEFAULT_TIMEOUT = 60
+
+# Canonical, nagios exit codes.
+STATE_OK = 0
+STATE_WARNING = 1
+STATE_CRITICAL = 2
+STATE_UNKNOWN = 3
+STATUS_MESSAGES = {
+    STATE_OK: 'OKAY:',
+    STATE_WARNING: 'WARNING:',
+    STATE_CRITICAL: 'CRITICAL:',
+    STATE_UNKNOWN: 'UNKNOWN:'
+}
+
+
+class Error(Exception):
+  """Base error class for this module."""
+
+
+class NagiosStateError(Error):
+  """A generic nagios status error."""
+  status_code = STATE_UNKNOWN
+  def __init__(self, message, status_code=None):
+    if status_code:
+      self.status_code = status_code
+    Error.__init__(self, message)
+
+
+class CriticalError(NagiosStateError):
+  """A critical error has occurred."""
+  status_code = STATE_CRITICAL
+
+
+class TimeoutError(Exception):
+  """A timeout has occurred."""
+
+
+def sock_connect(path):
+  """Creates a unix domain, stream socket and connects to path.
+
+  Args:
+    path: str, absolute path to unix socket name.
+  Returns:
+    AF_UNIX socket.socket of type SOCK_STREAM on success, None on error.
+  """
+  try:
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(path)
+    return sock
+  except socket.error:
+    return None
+
+
+def sock_sendcmd(sock, command):
+  """Writes command to socket.
+
+  Args:
+    sock: socket.socket, connected unix domain socket.
+    command: str, the command to send over socket.
+  Returns:
+    int, positive on success, less than or equal to zero on error.
+  """
+  sock.send(command + '\n')
+  status_message = sock_readline(sock)
+  if not status_message:
+    return 0
+  code, _ = status_message.split(' ', 1)
+  try:
+    return int(code)
+  except ValueError:
+    return 0
+
+
+def sock_readline(sock):
+  """Reads one line from sock, until first newline character.
+
+  Args:
+     sock: socket.socket, to read line from this socket.
+  Returns:
+     str, the line read, or empty string on error.
+  """
+  try:
+    data = ''
+    buf = []
+    while data != '\n':
+      data = sock.recv(1)
+      if not data:
+        break
+      if data != '\n':
+        buf.append(data)
+    return ''.join(buf)
+  except socket.error:
+    return ''
+
+
+def assert_collectd_installed():
+  """Asserts that collectd is installed in the utility slice.
+
+  Raises:
+    CriticalError, if an error occurs.
+  """
+  # Is collectd installed?
+  if not os.path.exists(COLLECTD_BIN):
+    raise CriticalError('collectd binary not present: %s.' % COLLECTD_BIN)
+
+  # Is collectd socket available?
+  if not os.path.exists(COLLECTD_UNIXSOCK):
+    raise CriticalError(
+        'collectd unixsock not present: %s.' % COLLECTD_UNIXSOCK)
+
+
+def assert_collectd_responds():
+  """Asserts that collectd is responding over the COLLECTD_UNIXSOCK.
+
+  Raises:
+    CriticalError if an error occurs.
+  """
+
+  # Is filesystem read-write ok?
+  if not os.access(COLLECTD_PID, os.W_OK):
+    raise CriticalError('collectd filesystem is NOT_WRITEABLE!')
+
+  # Is collectd responsive over unix socket?
+  sock = sock_connect(COLLECTD_UNIXSOCK)
+  if sock is None:
+    raise CriticalError(
+        'CONNECT_FAILED but unixsock present (%s)!' % COLLECTD_UNIXSOCK)
+
+  # Can we request a value over socket?
+  val = sock_sendcmd(sock, 'GETVAL "%s/meta/timer-read"' % HOSTNAME)
+  if val <= 0:
+    raise CriticalError(
+        'SENDCMD_FAILED but collectd unixsock is open.')
+
+  # Read as many lines as reported back from command.
+  for _ in xrange(0, val):
+    if not sock_readline(sock):
+      raise CriticalError('EMPTY_MESSAGE read from collectd socket.')
+
+
+def assert_collectd_vsys_setup():
+  """Asserts that vsys configuration is complete for mlab_utility.
+
+  Raises:
+    CriticalError if an error occurs.
+  """
+  # Is the vsys backend script installed?
+  if not os.path.exists(VSYSPATH_BACKEND):
+    raise CriticalError('NO_VSYS_BACKEND %s is missing!' % VSYSPATH_BACKEND)
+
+  # Is the vsys frontend FIFO in the slice context?
+  if not os.path.exists(VSYSPATH_SLICE):
+    raise CriticalError('NO_VSYS_SLICE %s is missing in slice' % VSYSPATH_SLICE)
+
+  # Is mlab_utility in the vsys acl for the backend script?
+  try:
+    acl = open(VSYSPATH_ACL, 'r').read().strip()
+  except IOError:
+    raise CriticalError('NO_VSYS_ACL cannot read vsys acl: %s' % VSYSPATH_ACL)
+
+  if SLICENAME not in acl:
+    raise CriticalError(
+        'NO_SLICE_IN_ACL %s is missing from ACL %s' % (SLICENAME, VSYSPATH_ACL))
+
+
+def run_collectd_nagios(host, metric, value, warning, critical):
+  """Runs collectd-nagios using given parameters as arguments.
+
+  Please see the collectd-nagios man page for documentation on the format of
+  'warning' and 'critical' thresholds.
+
+  Args:
+    host: str, experiment hostname.
+    metric: str, raw metric path. (e.g. network/if_octets-ipv6)
+    value: str, name of value within metric. (e.g. 'value', 'rx')
+    warning: str, a collectd-nagios warning threshold. (e.g. '0:20')
+    critical: str, a collectd-nagios critical threshold. (e.g. '0:30')
+  Returns:
+    exit code from collectd-nagios. Because this is a nagios plugin, these are
+        valid nagios exit states.
+  """
+  cmd = ('collectd-nagios -s {unixsock} -H {host} '+
+         '-n {metric} -d {value} -w {warning} '+
+         '-c {critical}')
+  cmd = cmd.format(unixsock=COLLECTD_UNIXSOCK, host=host,
+                   metric=metric, value=value, warning=warning,
+                   critical=critical)
+  child = subprocess.Popen(
+      cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  return child.wait()
+
+
+def assert_collectd_nagios_levels():
+  """Asserts actual values from collectd using collectd-nagios.
+
+  Asserts that storage quota is sufficient, metric collection is fast enough,
+  that CPU load is low, and memory usage is reasonable.
+
+  Raises:
+    NagiosStateError, if an error occurs.
+  """
+  # TODO: Make warning & critical thresholds configurable.
+
+  # Is utility slice quota ok?
+  exit_code = run_collectd_nagios(
+      'utility.mlab.'+HOSTNAME, 'storage/vs_quota_bytes-quota',
+      'used', '0:8000000000', '0:9000000000')
+  if exit_code != 0:
+    raise NagiosStateError('Storage quota usage is too high',
+                           status_code=exit_code)
+
+  # Is collectd cpu usage too high?
+  exit_code = run_collectd_nagios(
+      HOSTNAME, 'meta-collectd/process_cpu-system',
+      'value', '0:5', '0:10')
+  if exit_code != 0:
+    raise NagiosStateError('Collectd CPU usage is too high',
+                           status_code=exit_code)
+
+  # Actual memory usage should be pretty small, though vm size may be larger.
+  exit_code = run_collectd_nagios(
+      HOSTNAME, 'meta-collectd/process_memory-rss',
+      'value', '0:10000000', '0:15000000')
+  if exit_code != 0:
+    raise NagiosStateError('Collectd RSS memory usage is too high',
+                           status_code=exit_code)
+
+  # Is meta timer too high?
+  exit_code = run_collectd_nagios(
+      HOSTNAME, 'meta/timer-read',
+      'value', '0:6', '0:100')
+  if exit_code != 0:
+    raise NagiosStateError('Collectd mlab plugin taking too long to run',
+                           status_code=exit_code)
+
+
+def assert_disk_last_sync_time():
+  """Asserts the last sync time for vserver disk limits."""
+  # TODO: When was vserver quota last sync'd?
+  # TODO: How do we want to check this?
+  pass
+
+
+def check_collectd():
+  """Checks environment for signs of normal collectd-mlab behavior.
+  
+  Returns:
+    Tuple of (int, str), corresponding to the (nagios_state, error_message).
+  """
+  t_start = time.time()
+
+  # Check all critical conditions first.
+  try:
+    assert_collectd_installed()
+    assert_collectd_responds()
+    assert_collectd_vsys_setup()
+    assert_disk_last_sync_time()
+  except CriticalError as err:
+    return (err.status_code, str(err))
+
+  # Since the above establishes that collectd is working, now check collectd
+  # resource usage thresholds *from* collectd.
+  try:
+    assert_collectd_nagios_levels()
+  except NagiosStateError as err:
+    return (err.status_code, str(err))
+
+  # We've made it this far, so everything appears to be ok.
+  msg = 'Blue skies smiling at me: %0.2f sec' % (time.time() - t_start)
+  return (STATE_OK, msg)
+
+
+def init_alarm(timeout):
+  """Assigns a SIGALARM handler for timeout seconds."""
+  def handler(unused_signum, unused_frame):
+    """Raise a TimeoutError when called"""
+    raise TimeoutError('Timeout after %s' % timeout)
+  signal.signal(signal.SIGALRM, handler)
+  signal.alarm(timeout)
+
+
+def cancel_alarm():
+  """Cancels pending alarm."""
+  signal.alarm(0)
+
+
+def main():
+  init_alarm(DEFAULT_TIMEOUT)
+
+  try:
+    (status_code, message) = check_collectd()
+  except Exception as err:  # pylint: disable=W0703
+    status_code = STATE_UNKNOWN
+    message = str(err)
+
+  cancel_alarm()
+
+  print STATUS_MESSAGES.get(status_code, 'UNKNOWN:'), message
+  sys.exit(status_code)
+
+
+if __name__ == "__main__":
+  main()  # pragma: no cover.
---
+++ monitoring/check_collectd_mlab_test.py
@@ -0,0 +1,378 @@
+#!/usr/bin/python
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests for check_collectd_mlab check."""
+
+import os
+import time
+import socket
+import threading
+import unittest2 as unittest
+
+# Third-party modules.
+import mock
+
+# Module under test.
+import check_collectd_mlab
+
+# R0904: Too many public methods. Hard to avoid for unit tests.
+# pylint: disable=R0904
+
+
+class FakeCollectd(object):
+  """A fake version of collectd that listens to a unix domain socket."""
+
+  def __init__(self, unixsock):
+    self._unixsock = unixsock
+    self._sock = None
+    self._thread = threading.Thread(target=self._serve)
+    self._thread.daemon = True
+
+  def start(self):
+    """Starts the FakeCollectd service, listening on the unix domain socket."""
+    if os.path.exists(self._unixsock):
+      os.unlink(self._unixsock)
+    self._sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    self._sock.bind(self._unixsock)
+    self._sock.listen(1)
+    self._thread.start()
+
+  def shutdown(self):
+    """Waits up to 5 seconds for the FakeCollectd thread to shutdown.
+
+    Returns:
+      bool, True if shutdown succeeds, False if shutdown fails.
+    """
+    self._thread.join(5)
+    return not self._thread.isAlive()
+
+  def _serve(self):
+    """Runs a fake server that can respond to a single request.
+
+    This fake service emulates GETVAL with the option to inject errors.
+      https://collectd.org/wiki/index.php/Plain_text_protocol#GETVAL
+
+    The response is corrupted if the request contains one of these keywords.
+      send_bad_reply -- the number of values is bad.
+      send_empty_reply -- the request is received but no reply is sent.
+      close_after_reply -- the connection is closed after sending the number of
+          results to client.
+    """
+    # E1101: attribute access for nonexisting member. pylint 0.21.1 rejects
+    # the calls to sendall. Disable this check because the method is present.
+    # pylint: disable=E1101
+    # Wait for a connection
+    client, _ = self._sock.accept()
+    request = check_collectd_mlab.sock_readline(client)
+
+    # Send "number of values" response.
+    if 'send_bad_reply' in request:
+      client.sendall('not-a-number junk\n')
+    elif 'send_empty_reply' in request:
+      client.sendall('\n')
+    else:
+      client.sendall('1 junk\n')
+
+    # Send "actual values" response.
+    if 'close_after_reply' in request:
+      # Skip the response, so client read will fail.
+      client.close()
+      return
+    try:
+      client.sendall('default=1.0\n')
+    except socket.error:
+      # Incase of sigpipe due to client closing connection first.
+      pass
+    # pylint: enable=E1101
+    client.close()
+    self._sock.close()
+
+
+class MlabNagiosCollectdTests(unittest.TestCase):
+
+  def setUp(self):
+    self._testdata_dir = os.path.join(
+        os.path.dirname(check_collectd_mlab.__file__), 'testdata')
+    self.fake_socket = os.path.join(self._testdata_dir, 'fake_socket')
+    self.fake_collectd = FakeCollectd(self.fake_socket)
+    self.fake_collectd.start()
+
+  def tearDown(self):
+    self.assertTrue(self.fake_collectd.shutdown())
+
+  def testunit_sock_sendcmd(self):
+    check_collectd_mlab.HOSTNAME = 'my.hostname'
+    sock = check_collectd_mlab.sock_connect(self.fake_socket)
+
+    returned_value = check_collectd_mlab.sock_sendcmd(sock, 'GETVAL "whatever"')
+
+    self.assertEqual(returned_value, 1)
+
+  def testunit_sock_sendcmd_WHEN_receive_bad_reply_RETURNS_zero(self):
+    check_collectd_mlab.HOSTNAME = 'my.hostname'
+    sock = check_collectd_mlab.sock_connect(self.fake_socket)
+
+    returned_value = check_collectd_mlab.sock_sendcmd(sock, 'send_bad_reply')
+
+    self.assertEqual(returned_value, 0)
+
+  def testunit_assert_collectd_responds_WHEN_sock_sendcmd_fails(self):
+    check_collectd_mlab.COLLECTD_PID = (
+        os.path.join(self._testdata_dir, 'fake_pid'))
+    check_collectd_mlab.COLLECTD_UNIXSOCK = self.fake_socket
+    check_collectd_mlab.HOSTNAME = 'send_empty_reply'
+
+    try:
+      check_collectd_mlab.assert_collectd_responds()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('SENDCMD_FAILED' in str(err))
+
+  def testunit_assert_collectd_responds_WHEN_sock_readline_fails(self):
+    check_collectd_mlab.COLLECTD_PID = (
+        os.path.join(self._testdata_dir, 'fake_pid'))
+    check_collectd_mlab.COLLECTD_UNIXSOCK = self.fake_socket
+    check_collectd_mlab.HOSTNAME = 'close_after_reply'
+
+    try:
+      check_collectd_mlab.assert_collectd_responds()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('EMPTY_MESSAGE' in str(err))
+
+
+class MlabNagiosTests(unittest.TestCase):
+
+  def setUp(self):
+    self._testdata_dir = os.path.join(
+        os.path.dirname(check_collectd_mlab.__file__), 'testdata')
+
+  def testunit_sock_connect_WHEN_no_socket_RETURNS_None(self):
+    returned_socket = check_collectd_mlab.sock_connect('no_socket_name')
+
+    self.assertEqual(returned_socket, None)
+
+  def testunit_sock_readline_WHEN_socket_error_RETURNS_empty_string(self):
+    mock_sock = mock.Mock(spec_set=socket.socket)
+    mock_sock.recv.side_effect = socket.error('fake error')
+
+    returned_value = check_collectd_mlab.sock_readline(mock_sock)
+
+    self.assertEqual(returned_value, '')
+    self.assertTrue(mock_sock.recv.called)
+
+  def testunit_assert_collectd_installed_WHEN_bin_missing_RAISES_CriticalError(
+      self):
+    check_collectd_mlab.COLLECTD_BIN = 'does_not_exist'
+
+    try:
+      check_collectd_mlab.assert_collectd_installed()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('does_not_exist' in str(err))
+
+  def testunit_assert_collectd_installed_WHEN_bad_socket_RAISES_CriticalError(
+      self):
+    check_collectd_mlab.COLLECTD_BIN = (
+        os.path.join(self._testdata_dir, 'fake_bin'))
+    check_collectd_mlab.COLLECTD_UNIXSOCK = 'does_not_exist'
+
+    try:
+      check_collectd_mlab.assert_collectd_installed()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('does_not_exist' in str(err))
+
+  @mock.patch('os.access')
+  def testunit_assert_collectd_responds_WHEN_collectd_not_writeable(
+      self, mock_access):
+    # Mock out os.access to guarantee that write access is rejected.
+    mock_access.return_value = False
+
+    try:
+      check_collectd_mlab.assert_collectd_responds()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('NOT_WRITEABLE' in str(err))
+    self.assertTrue(mock_access.called)
+
+  def testunit_assert_collectd_responds_WHEN_sock_connect_fails(self):
+    check_collectd_mlab.COLLECTD_PID = (
+        os.path.join(self._testdata_dir, 'fake_pid'))
+    check_collectd_mlab.COLLECTD_UNIXSOCK = 'does_not_exist'
+
+    try:
+      check_collectd_mlab.assert_collectd_responds()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('CONNECT_FAILED' in str(err))
+
+  def testunit_assert_collectd_vsys_setup_WHEN_vsys_backend_is_missing(self):
+    check_collectd_mlab.VSYSPATH_BACKEND = 'does_not_exist'
+
+    try:
+      check_collectd_mlab.assert_collectd_vsys_setup()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('does_not_exist' in str(err))
+
+  def testunit_assert_collectd_vsys_setup_WHEN_vsys_slice_is_missing(self):
+    check_collectd_mlab.VSYSPATH_BACKEND = os.path.join(
+        self._testdata_dir, 'fake_backend')
+    check_collectd_mlab.VSYSPATH_SLICE = 'does_not_exist'
+
+    try:
+      check_collectd_mlab.assert_collectd_vsys_setup()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('NO_VSYS_SLICE' in str(err))
+
+  def testunit_assert_collectd_vsys_setup_WHEN_vsys_acl_is_missing(self):
+    check_collectd_mlab.VSYSPATH_BACKEND = os.path.join(
+        self._testdata_dir, 'fake_backend')
+    check_collectd_mlab.VSYSPATH_SLICE = os.path.join(
+        self._testdata_dir, 'fake_slice')
+    check_collectd_mlab.VSYSPATH_ACL = 'does_not_exist'
+
+    try:
+      check_collectd_mlab.assert_collectd_vsys_setup()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('NO_VSYS_ACL' in str(err))
+
+  def testunit_assert_collectd_vsys_setup_WHEN_acl_incomplete(self):
+    check_collectd_mlab.VSYSPATH_BACKEND = os.path.join(
+        self._testdata_dir, 'fake_backend')
+    check_collectd_mlab.VSYSPATH_SLICE = os.path.join(
+        self._testdata_dir, 'fake_slice')
+    check_collectd_mlab.VSYSPATH_ACL = os.path.join(
+        self._testdata_dir, 'fake_acl')
+
+    try:
+      check_collectd_mlab.assert_collectd_vsys_setup()
+      self.fail()  # pragma: no cover.
+    except check_collectd_mlab.CriticalError as err:
+      self.assertTrue('NO_SLICE_IN_ACL' in str(err))
+
+  @mock.patch('subprocess.Popen')
+  def testcover_run_collectd_nagios(self, mock_popen):
+    mock_popen.return_value.wait.return_value = 2
+
+    returned_value = check_collectd_mlab.run_collectd_nagios(
+        'host', 'metric', 'value', 'warning', 'critical')
+
+    self.assertEqual(returned_value, 2)
+    self.assertTrue(mock_popen.called)
+
+  @mock.patch('check_collectd_mlab.run_collectd_nagios')
+  def testcover_assert_collectd_nagios_levels(self, mock_run_collectd_nagios):
+    # This is not ideal. But, it's just a coverage test.
+    # Non-zero values cause a failue. Cause each call to to fail in sequence.
+    mock_run_collectd_nagios.side_effect = [1]
+    self.assertRaises(check_collectd_mlab.NagiosStateError,
+                      check_collectd_mlab.assert_collectd_nagios_levels)
+
+    mock_run_collectd_nagios.side_effect = [0, 1]
+    self.assertRaises(check_collectd_mlab.NagiosStateError,
+                      check_collectd_mlab.assert_collectd_nagios_levels)
+
+    mock_run_collectd_nagios.side_effect = [0, 0, 1]
+    self.assertRaises(check_collectd_mlab.NagiosStateError,
+                      check_collectd_mlab.assert_collectd_nagios_levels)
+
+    mock_run_collectd_nagios.side_effect = [0, 0, 0, 1]
+    self.assertRaises(check_collectd_mlab.NagiosStateError,
+                      check_collectd_mlab.assert_collectd_nagios_levels)
+
+  def teststub_assert_disk_last_sync_time(self):
+    check_collectd_mlab.assert_disk_last_sync_time()
+
+  @mock.patch('check_collectd_mlab.assert_collectd_installed')
+  @mock.patch('check_collectd_mlab.assert_collectd_responds')
+  @mock.patch('check_collectd_mlab.assert_collectd_vsys_setup')
+  @mock.patch('check_collectd_mlab.assert_collectd_nagios_levels')
+  @mock.patch('check_collectd_mlab.assert_disk_last_sync_time')
+  def testcover_check_collectd(
+      self, mock_last_sync_time, mock_collectd_nagios_levels, mock_vsys_setup,
+      mock_collectd_responds, mock_collectd_installed):
+
+    (state, _) = check_collectd_mlab.check_collectd()
+
+    self.assertEqual(state, check_collectd_mlab.STATE_OK)
+    self.assertTrue(mock_collectd_installed.called)
+    self.assertTrue(mock_collectd_responds.called)
+    self.assertTrue(mock_vsys_setup.called)
+    self.assertTrue(mock_collectd_nagios_levels.called)
+    self.assertTrue(mock_last_sync_time.called)
+
+  @mock.patch('check_collectd_mlab.assert_collectd_installed')
+  @mock.patch('check_collectd_mlab.assert_collectd_responds')
+  @mock.patch('check_collectd_mlab.assert_collectd_vsys_setup')
+  @mock.patch('check_collectd_mlab.assert_collectd_nagios_levels')
+  @mock.patch('check_collectd_mlab.assert_disk_last_sync_time')
+  def testcover_check_collectd_WHEN_nagios_error(
+      self, mock_last_sync_time, mock_collectd_nagios_levels, mock_vsys_setup,
+      mock_collectd_responds, mock_collectd_installed):
+    mock_collectd_nagios_levels.side_effect = (
+        check_collectd_mlab.NagiosStateError('error'))
+
+    (state, message) = check_collectd_mlab.check_collectd()
+
+    self.assertEqual(state, check_collectd_mlab.STATE_UNKNOWN)
+    self.assertEqual(message, 'error')
+    self.assertTrue(mock_collectd_installed.called)
+    self.assertTrue(mock_collectd_responds.called)
+    self.assertTrue(mock_vsys_setup.called)
+    self.assertTrue(mock_collectd_nagios_levels.called)
+    self.assertTrue(mock_last_sync_time.called)
+
+  @mock.patch('check_collectd_mlab.assert_collectd_installed')
+  def testcover_check_collectd_WHEN_state_critical(
+      self, mock_collectd_installed):
+    mock_collectd_installed.side_effect = (
+        check_collectd_mlab.CriticalError('fail'))
+
+    (state, _) = check_collectd_mlab.check_collectd()
+
+    self.assertEqual(state, check_collectd_mlab.STATE_CRITICAL)
+    self.assertTrue(mock_collectd_installed.called)
+
+  def testunit_alarm(self):
+    check_collectd_mlab.init_alarm(1)
+    try:
+      time.sleep(5)
+      self.fail('Alarm did not trigger.')  # pragma: no cover.
+    except check_collectd_mlab.TimeoutError:
+      pass
+    check_collectd_mlab.cancel_alarm()
+
+  @mock.patch('sys.stdout')
+  @mock.patch('check_collectd_mlab.check_collectd')
+  def testcover_main(self, mock_check_status, mock_stdout):
+    mock_check_status.return_value = (check_collectd_mlab.STATE_OK, 'ok')
+    self.assertRaises(SystemExit, check_collectd_mlab.main)
+    self.assertTrue(len(mock_stdout.mock_calls) > 0)
+    self.assertTrue(mock_check_status.called)
+
+  @mock.patch('sys.stdout')
+  @mock.patch('check_collectd_mlab.check_collectd')
+  def testcover_main_WHEN_timeout(self, mock_check_status, mock_stdout):
+    mock_check_status.side_effect = check_collectd_mlab.TimeoutError('timeout')
+    self.assertRaises(SystemExit, check_collectd_mlab.main)
+    self.assertTrue(len(mock_stdout.mock_calls) > 0)
+    self.assertTrue(mock_check_status.called)
+
+
+if __name__ == "__main__":
+  unittest.main()
